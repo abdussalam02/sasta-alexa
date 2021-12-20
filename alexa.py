28d2ab423b1f44efd90f409332d0afd9de897a79
abdussalam02 <samaniabdussalam02@gmail.com>
2021-12-20 15:44:38 +0530

    assigning values to variables

--- alexa.py
+++ alexa.py
@@ -5,3 +5,9 @@ import pywhatkit
 import datetime as dt
 import wikipedia as wk
 import pyjokes
+
+# defining variables
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
